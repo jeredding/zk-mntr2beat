0e8a4fd794d76c13a9a63d0b7e79869a0907a791
Erik Redding <erik@elastic.co>
2016-04-13 12:31:28 -0500

    remove separate data file to merge in with mntr; include metricbeats available.

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"os"
-
+    "github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/metricbeat/beater"
+    _ "github.com/elastic/beats/metricbeat/include"
 	_ "github.com/jeredding/zk-mntr2beat/module/zookeeper/mntr"
 	_ "github.com/jeredding/zk-mntr2beat/module/zookeeper"
-
-	"github.com/elastic/beats/libbeat/beat"
 )
 
 var Name = "zk-mntr2beat"
--- module/zookeeper/mntr/mntr.go
+++ module/zookeeper/mntr/mntr.go
@@ -1,10 +1,14 @@
 package mntr
 
 import (
-    "net"
-    "time"
+    "bufio"
     "bytes"
+    "io"
     "io/ioutil"
+    "net"
+    "regexp"
+    "strconv"
+    "time"
     "github.com/elastic/beats/libbeat/logp"
     "github.com/elastic/beats/libbeat/common"
     "github.com/elastic/beats/metricbeat/helper"
@@ -81,4 +85,147 @@ func (m *MetricSeter) Fetch(ms *helper.MetricSet, host string) (event common.Map
     }
 
     return eventMapping(bytes.NewReader(result)), nil
+}
+
+func eventMapping(response io.Reader) common.MapStr {
+
+    var (
+        versionString           string
+        avgLatency              int
+        minLatency              int
+        maxLatency              int
+        packetsReceived         int
+        packetsSent             int
+        numAliveConnections     int
+        outstandingRequests     int
+        serverState             int
+        znodeCount              int
+        watchCount              int
+        ephemeralsCount         int
+        approximateDataSize     int
+        openFileDescriptorCount int
+        maxFileDescriptorCount  int
+        followers               int
+        syncedFollowers         int
+        pendingSyncs            int
+    )
+
+    //  zk_version      3.5.1-alpha-1693007, built on 07/28/2015 07:19 GMT
+    //  zk_avg_latency  0
+    //  zk_max_latency  1789
+    //  zk_min_latency  0
+    //  zk_packets_received     22152032
+    //  zk_packets_sent 30959914
+    //  zk_num_alive_connections        1033
+    //  zk_outstanding_requests 0
+    //  zk_server_state leader
+    //  zk_znode_count  242609
+    //  zk_watch_count  940522
+    //  zk_ephemerals_count     8565
+    //  zk_approximate_data_size        372143564
+    //  zk_open_file_descriptor_count   1083
+    //  zk_max_file_descriptor_count    1048576
+    //  zk_followers    5
+    //  zk_synced_followers     2
+    //  zk_pending_syncs        0
+
+    var re *regexp.Regexp
+    scanner := bufio.NewScanner(response)
+    for scanner.Scan() {
+
+        re = regexp.MustCompile("zk_version\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            versionString = matches[1]
+        }
+        re = regexp.MustCompile("zk_avg_latency\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            avgLatency, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_max_latency\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            maxLatency, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_min_latency\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            minLatency, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_packets_received\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            packetsReceived, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_packets_sent\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            packetsSent, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_num_alive_connections\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            numAliveConnections, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_outstanding_requests\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            outstandingRequests, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_server_state\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            serverState, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_znode_count\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            znodeCount, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_watch_count\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            watchCount, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_ephemerals_count\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            ephemeralsCount, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_approximate_data_size\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            approximateDataSize, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_open_file_descriptor_count\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            openFileDescriptorCount, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_max_file_descriptor_count\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            maxFileDescriptorCount, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_followers\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            znodeCount, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_synced_followers\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            syncedFollowers, _ = strconv.Atoi(matches[1])
+        }
+        re = regexp.MustCompile("zk_pending_syncs\\s+(.*$)")
+        if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+            pendingSyncs, _ = strconv.Atoi(matches[1])
+        }
+    }
+
+    event := common.MapStr{
+        "version_string":             versionString,
+        "avg_latency":                avgLatency,
+        "min_latency":                minLatency,
+        "max_latency":                maxLatency,
+        "packets_received":           packetsReceived,
+        "packets_sent":               packetsSent,
+        "num_alive_connections":      numAliveConnections,
+        "outstanding_requests":       outstandingRequests,
+        "server_state":               serverState,
+        "znode_count":                znodeCount,
+        "watch_count":                watchCount,
+        "ephemerals_count":           ephemeralsCount,
+        "approximate_data_size":      approximateDataSize,
+        "open_file_descriptor_count": openFileDescriptorCount,
+        "max_file_descriptor_count":  maxFileDescriptorCount,
+        "followers":                  followers,
+        "synced_followers":           syncedFollowers,
+        "pending_syncs":              pendingSyncs,
+    }
+    return event
 }
